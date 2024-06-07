f8cf9bb23c7705c6e4b3c409ec31519dd52a683a
4lph4 <b4rryd4le@gmail.com>
2024-06-07 12:58:34 +0000

    Heroku asgi debug commit

--- s_carpool/asgi.py
+++ s_carpool/asgi.py
@@ -7,20 +7,31 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-import os, django
+import os
 
+# Set the default Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "s_carpool.settings")
+
+# Add diagnostic print statement
+print(f"DJANGO_SETTINGS_MODULE: {os.getenv('DJANGO_SETTINGS_MODULE')}")
+
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from core_api.middleware import TokenAuthMiddleware
 import core_api.routing
 
-# from core_api.middleware import TokenAuthMiddleware
+# Initialize Django
+try:
+    django.setup()
+    print("Django settings loaded successfully.")
+except Exception as e:
+    print(f"Error loading Django settings: {e}")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "s_carpool.settings")
-django.setup()
+# Define the authentication middleware
 auth = TokenAuthMiddleware(URLRouter(core_api.routing.websocket_urlpatterns))
 
+# Define the ASGI application
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
