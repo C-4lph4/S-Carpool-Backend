902c0763cfe0f7fa70ebec85fb73a1491267882a
4lph4 <b4rryd4le@gmail.com>
2024-06-07 16:25:56 +0000

    update signup and code generation

--- core_api/send.py
+++ core_api/send.py
@@ -3,7 +3,6 @@ import random
 import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import threading
 
 sender = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
@@ -25,39 +24,17 @@ def send_code(receiver):
     body = f"Here is your activation code: \n <h2>{code}</h2>"
     message.attach(MIMEText(body, "html"))
 
-    timeout = 40
+    def send_mail(sender, receiver, message):
+        try:
+            with smtplib.SMTP("smtp.gmail.com", 587) as server:
+                server.starttls()
+                server.login(sender, password)
+                server.sendmail(sender, receiver, message.as_string())
+            print("Email sent successfully")
+        except Exception as e:
+            print(e)
+            print("Failed to send email")
 
-    email_sent_event = threading.Event()
-    email_status = {"success": None}
+    send_mail(sender, receiver, message)
 
-    def send_mail_and_set_event(
-        sender, receiver, message, email_status, email_sent_event
-    ):
-        email_status["success"] = send_mail(sender, receiver, message)
-        email_sent_event.set()
-
-    timer = threading.Timer(
-        timeout,
-        send_mail_and_set_event,
-        args=[sender, receiver, message, email_status, email_sent_event],
-    )
-    timer.start()
-
-    email_sent_event.wait(timeout)
-
-    if email_status["success"]:
-        return code
-    else:
-        return
-
-
-def send_mail(sender, receiver, message):
-    try:
-        with smtplib.SMTP("smtp.gmail.com", 587) as server:
-            server.starttls()
-            server.login(sender, password)
-            server.sendmail(sender, receiver, message.as_string())
-        return True
-    except Exception as e:
-        print(e)
-        return False
+    return code
